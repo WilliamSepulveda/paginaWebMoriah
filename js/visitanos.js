8c4c13a1d7d02c43984fc2faf9503dd4cdaa0af7
William Sepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2025-02-05 22:33:59 -0500

    feat :lipstick: responsividad de sección de (visitanos)

--- js/visitanos.js
+++ js/visitanos.js
@@ -38,3 +38,28 @@ function startCountdowns() {
 }
 
 startCountdowns();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMoriah = document.getElementById('navMoriah');
+    const overlay = document.getElementById('menu-overlay');
+    const menuBtn = document.getElementById('menu-btn');
+
+    function toggleMenu() {
+        const isOpen = navMoriah.classList.contains('open');
+
+        if (isOpen) {
+            navMoriah.classList.remove('open');
+            overlay.classList.remove('active');
+            menuToggle.checked = false; // Desmarcar el checkbox
+        } else {
+            navMoriah.classList.add('open');
+            overlay.classList.add('active');
+            menuToggle.checked = true; // Marcar el checkbox
+        }
+    }
+
+    menuBtn.addEventListener('click', toggleMenu);
+    overlay.addEventListener('click', toggleMenu);
+});
+
