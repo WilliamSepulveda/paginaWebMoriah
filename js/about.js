70b0892ea5502fde8a793d1668cde75141fa5e63
William Sepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2025-02-18 21:00:33 -0500

    feat :lipstick: ajustes de diseño en progreso

--- js/about.js
+++ js/about.js
@@ -1,8 +1,2 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Importar los estilos de AOS
-AOS.init({
-  duration: 1200, // Duración de la animación
-  once: false, // Las animaciones se pueden repetir
-  easing: 'ease-in-out', // Tipo de aceleración
-  offset: 100, // Iniciar la animación cuando el elemento esté a 100px de la vista
-});
+
+
--- js/intersecion.js
+++ js/intersecion.js
@@ -33,3 +33,28 @@ document.addEventListener("DOMContentLoaded", function () {
     button.classList.remove("pulse");
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const navMoriah = document.getElementById('navMoriah');
+  const overlay = document.getElementById('menu-overlay');
+  const menuBtn = document.getElementById('menu-btn');
+
+  function toggleMenu() {
+      const isOpen = navMoriah.classList.contains('open');
+
+      if (isOpen) {
+          navMoriah.classList.remove('open');
+          overlay.classList.remove('active');
+          menuToggle.checked = false; // Desmarcar el checkbox
+      } else {
+          navMoriah.classList.add('open');
+          overlay.classList.add('active');
+          menuToggle.checked = true; // Marcar el checkbox
+      }
+  }
+
+  menuBtn.addEventListener('click', toggleMenu);
+  overlay.addEventListener('click', toggleMenu);
+});
