ba1a66a42eace0201f820bc932b2703df6517213
William Sepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2025-03-17 22:08:46 -0500

    feat :lipstick: se hacen banner's de jóvenes y ministerios de alabanza
    importados imagenes necesarias y creando los diseño de los banner's

---
+++ js/Alabanza.js
@@ -0,0 +1,46 @@
+// seccion de checkbox
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMoriah = document.getElementById('navMoriah');
+    const overlay = document.getElementById('menu-overlay');
+    const menuBtn = document.getElementById('menu-btn');
+
+    function toggleMenu() {
+        const isOpen = navMoriah.classList.contains('open');
+
+        if (isOpen) {
+            navMoriah.classList.remove('open');
+            overlay.classList.remove('active');
+            menuToggle.checked = false; // Desmarcar el checkbox
+        } else {
+            navMoriah.classList.add('open');
+            overlay.classList.add('active');
+            menuToggle.checked = true; // Marcar el checkbox
+        }
+    }
+
+    menuBtn.addEventListener('click', toggleMenu);
+    overlay.addEventListener('click', toggleMenu);
+});
+
+
+// seccion de banner
+import Spheres2Background from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.8/build/backgrounds/spheres2.cdn.min.js'
+
+const bg = Spheres2Background(document.getElementById('webgl-canvas'), {
+  count: 200,
+  colors: [0xff0000, 0x0, 0xffffff],
+  minSize: 0.05,
+  maxSize: 0.8
+})
+
+const button1 = document.getElementById('colors-btn')
+
+document.body.addEventListener('click', (ev) => {
+  if (ev.target !== button1) bg.togglePause()
+})
+
+button1.addEventListener('click', () => {
+  bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
+  bg.spheres.light1.color.set(0xffffff * Math.random())
+})
